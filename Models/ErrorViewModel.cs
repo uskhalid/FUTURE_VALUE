b4a513b4b130f1282636e55e6c24776771bf49b2
786TOUSEEF <71210711+786TOUSEEF@users.noreply.github.com>
2022-03-11 14:02:38 -0800

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FUTURE_VALUE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
